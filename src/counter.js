259e3c0ffcd8371b2aeb55cb6d3ce265e860f8a8
Mirza Mubeen Baig <mubeenbaig30gb@gmail.com>
2022-12-14 12:49:38 +0500

    React Hooks

---
+++ src/counter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Counter = (props) => {
+    return(
+        <>
+            <h2>Value of Counter Varialble is {props.counter}</h2>
+        </>
+    );
+}
+
+export default Counter;
